dba204e68adb69c7351bab6eac0f87d20cacda76
Dongmin Kim <ddmkim94@gmail.com>
2024-01-16 14:41:20 +0900

    프로젝트 생성

---
+++ src/main/java/com/api/farmingsoon/FarmingsoonApplication.java
@@ -0,0 +1,13 @@
+package com.api.farmingsoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmingsoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmingsoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/farmingsoon/FarmingsoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.farmingsoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmingsoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
