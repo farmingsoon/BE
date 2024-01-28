c5b70ffb1e5db22f31871b179f36d9995724040e
Dongmin Kim <ddmkim94@gmail.com>
2024-01-28 13:51:52 +0900

    Feat: 좋아요를 누른 상품 목록 기능 구현
    - 내가 좋아요를 누른 상품의 목록을 조회하는 API 구현

--- src/main/java/com/api/farmingsoon/domain/item/repository/ItemRepository.java
+++ src/main/java/com/api/farmingsoon/domain/item/repository/ItemRepository.java
@@ -1,8 +1,13 @@
 package com.api.farmingsoon.domain.item.repository;
 
 import com.api.farmingsoon.domain.item.domain.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
 
+    Page<Item> findAllByIdIn(List<Long> itemIds, Pageable pageable);
 }
