c3b0d3fb1ece032f3661f6d4c5dfc5b6127b731c
seungnYong <77851079+gkfktkrh153@users.noreply.github.com>
2024-02-03 04:32:57 +0900

    Update FarmingsoonApplicationTests.java

--- src/test/java/com/api/farmingsoon/FarmingsoonApplicationTests.java
+++ src/test/java/com/api/farmingsoon/FarmingsoonApplicationTests.java
@@ -3,7 +3,7 @@ package com.api.farmingsoon;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class FarmingsoonApplicationTests {
 
 	@Test
