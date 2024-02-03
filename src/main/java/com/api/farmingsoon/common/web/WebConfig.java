66e984a826f05592f66a0d17e5250b639bb0c7a6
gkfktkrh153 <gkfktkrh153@naver.com>
2024-02-04 00:13:24 +0900

    CORS

--- src/main/java/com/api/farmingsoon/common/web/WebConfig.java
+++ src/main/java/com/api/farmingsoon/common/web/WebConfig.java
@@ -19,6 +19,24 @@ public class WebConfig implements WebMvcConfigurer {
     private final List<String> excludePointList = Arrays.asList();
 
     private final List<String> addEndPointList = Arrays.asList("/api/**");
+    /**
+     * @Description
+     * CORS설정
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                        .exposedHeaders("accessToken", "refreshToken");
+                //.allowCredentials(true); // .allowedOriginPatterns("*") 이렇게 와일드 카드로 설정하면 이거 쓰면 에러남 ( 실행 조차  X )
+            }
+        };
+    }
 
     /**
      * @Description
