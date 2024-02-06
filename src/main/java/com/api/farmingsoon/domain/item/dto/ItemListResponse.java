441176f05b3421f968b1e27edac3a2c6f17e6b0b
gkfktkrh153 <gkfktkrh153@naver.com>
2024-02-06 20:05:43 +0900

    DTO 기본생성자 추가

--- src/main/java/com/api/farmingsoon/domain/item/dto/ItemListResponse.java
+++ src/main/java/com/api/farmingsoon/domain/item/dto/ItemListResponse.java
@@ -6,17 +6,24 @@ import com.api.farmingsoon.domain.like.model.LikeableItem;
 import com.api.farmingsoon.domain.member.model.Member;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class ItemListResponse {
 
     private List<ItemResponse> items; // 상품 데이터
     private Pagination pagination; // 페이지 관련 데이터
+
+    @Builder
+    public ItemListResponse(List<ItemResponse> items, Pagination pagination) {
+        this.items = items;
+        this.pagination = pagination;
+    }
 
     public static ItemListResponse of(Page<Item> itemPage, Member viewer) {
         Page<ItemResponse> itemDtoPage = itemPage.map(item -> ItemResponse.of(item, viewer)); // Page<Item> -> Page<ItemDto>
@@ -27,7 +34,7 @@ public class ItemListResponse {
     }
 
     @Getter
-    @Builder
+    @NoArgsConstructor
     public static class ItemResponse {
 
         private Long itemId; // 상품 접근
@@ -43,6 +50,22 @@ public class ItemListResponse {
         private Integer viewCount;
         private String thumbnailImgUrl;
         private Boolean likeStatus;
+        @Builder
+        public ItemResponse(Long itemId, String title, String description, LocalDateTime expiredAt, Integer highestPrice, Integer hopePrice, Integer lowestPrice, String itemStatus, Integer bidCount, Integer likeCount, Integer viewCount, String thumbnailImgUrl, Boolean likeStatus) {
+            this.itemId = itemId;
+            this.title = title;
+            this.description = description;
+            this.expiredAt = expiredAt;
+            this.highestPrice = highestPrice;
+            this.hopePrice = hopePrice;
+            this.lowestPrice = lowestPrice;
+            this.itemStatus = itemStatus;
+            this.bidCount = bidCount;
+            this.likeCount = likeCount;
+            this.viewCount = viewCount;
+            this.thumbnailImgUrl = thumbnailImgUrl;
+            this.likeStatus = likeStatus;
+        }
 
         private static ItemResponse of(Item item, Member viewer) {
 
@@ -65,7 +88,7 @@ public class ItemListResponse {
     }
 
     @Getter
-    @Builder
+    @NoArgsConstructor
     public static class Pagination {
 
         private int totalPageSize; // 전체 페이지수
@@ -76,8 +99,19 @@ public class ItemListResponse {
         private int pageSize; // 현재 페이지의 전체 사이즈
         private int elementSize; // 현재 페이지에 있는 요소의 수
 
+        @Builder
+        public Pagination(int totalPageSize, long totalElementSize, int page, boolean hasNext, boolean hasPrevious, int pageSize, int elementSize) {
+            this.totalPageSize = totalPageSize;
+            this.totalElementSize = totalElementSize;
+            this.page = page;
+            this.hasNext = hasNext;
+            this.hasPrevious = hasPrevious;
+            this.pageSize = pageSize;
+            this.elementSize = elementSize;
+        }
+
         private static Pagination of(Page<ItemResponse> itemDtoPage) {
-            return builder()
+            return Pagination.builder()
                     .totalPageSize(itemDtoPage.getTotalPages())
                     .totalElementSize(itemDtoPage.getTotalElements())
                     .page(itemDtoPage.getNumber() + 1)
