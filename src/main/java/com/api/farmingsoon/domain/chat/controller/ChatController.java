4ef081d952fb19832c7f1f65bbde47bf9fcb93a3
gkfktkrh153 <gkfktkrh153@naver.com>
2024-02-15 17:04:01 +0900

    채팅 권한 세팅 문제 처리

--- src/main/java/com/api/farmingsoon/domain/chat/controller/ChatController.java
+++ src/main/java/com/api/farmingsoon/domain/chat/controller/ChatController.java
@@ -1,12 +1,17 @@
 package com.api.farmingsoon.domain.chat.controller;
 
 import com.api.farmingsoon.common.annotation.LoginChecking;
+import com.api.farmingsoon.common.security.jwt.JwtProvider;
+import com.api.farmingsoon.common.util.JwtUtils;
 import com.api.farmingsoon.domain.chat.dto.ChatMessageRequest;
 import com.api.farmingsoon.domain.chat.dto.ChatResponse;
 import com.api.farmingsoon.domain.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,10 +19,19 @@ import org.springframework.stereotype.Controller;
 public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatService chatService;
+    private final JwtProvider jwtProvider;
 
-    @LoginChecking
     @MessageMapping("/chat/message")
-    public void sendMessage(ChatMessageRequest chatMessageRequest) {
+    public void sendMessage(ChatMessageRequest chatMessageRequest, @Header("Authorization") String accessToken) {
+        String ac = JwtUtils.extractBearerToken(accessToken);
+
+        if (ac != null) { // 토큰 재발급의 요청이 아니면서 accessToken이 존재할 때
+            if (jwtProvider.validateAccessToken(ac)) { // 토큰이 유효한 경우 and 로그인 상태
+                Authentication authentication = jwtProvider.getAuthenticationByAccessToken(ac);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
         ChatResponse chatResponse = chatService.create(chatMessageRequest);
         messagingTemplate.convertAndSend("/sub/chat-room/" + chatMessageRequest.getChatRoomId(), chatResponse);
     }
