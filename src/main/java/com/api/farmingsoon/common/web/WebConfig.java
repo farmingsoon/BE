e917414efcaa6c04b18cc254acf76695b5a3c59f
seungnYong <77851079+gkfktkrh153@users.noreply.github.com>
2024-02-17 17:20:48 +0900

    Rollback

--- src/main/java/com/api/farmingsoon/common/web/WebConfig.java
+++ src/main/java/com/api/farmingsoon/common/web/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import jakarta.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,8 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
      * CORS설정
      */
     @Bean
-    public WebMvcConfigurer corsConfigurer(HttpServletRequest request) {
-        System.out.println(request.getHeader("Origin"));
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
