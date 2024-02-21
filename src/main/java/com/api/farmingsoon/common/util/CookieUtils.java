5e724fb61a66c783b321fd9aa019ad23c2d3cc3f
gkfktkrh153 <gkfktkrh153@naver.com>
2024-02-21 21:43:19 +0900

    서브 도메인도 혀용

--- src/main/java/com/api/farmingsoon/common/util/CookieUtils.java
+++ src/main/java/com/api/farmingsoon/common/util/CookieUtils.java
@@ -44,7 +44,7 @@ public class CookieUtils {
                 .path("/")
                 .sameSite("None")
                 .httpOnly(true)
-                .domain("server.farmingsoon.site")
+                .domain(".farmingsoon.site")
                 .secure(true)
                 .maxAge(TimeUtils.getRemainingTimeUntilMidnight())
                 .build();
