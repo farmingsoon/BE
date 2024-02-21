bd8f079c9915d2354f2ccb204bf8487fc2b6f49d
gkfktkrh153 <gkfktkrh153@naver.com>
2024-02-21 17:24:34 +0900

    LoginResponse DTO스펙변경

--- src/main/java/com/api/farmingsoon/domain/member/dto/LoginResponse.java
+++ src/main/java/com/api/farmingsoon/domain/member/dto/LoginResponse.java
@@ -9,14 +9,18 @@ import lombok.Getter;
 public class LoginResponse {
 
     private final Long memberId;
+    private final String nickname;
+    private final String profileImgUrl;
     private final String tokenType;
     private final String accessToken;
     private final String refreshToken;
 
     @Builder
-    private LoginResponse(String accessToken, String refreshToken, Long memberId){
+    private LoginResponse(String accessToken, String refreshToken, String nickname, String profileImgUrl, Long memberId){
         this.memberId = memberId;
         this.tokenType = "Bearer";
+        this.nickname = nickname;
+        this.profileImgUrl = profileImgUrl;
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
     }
@@ -24,6 +28,8 @@ public class LoginResponse {
     public static LoginResponse of(JwtToken jwtToken, Member member) {
         return LoginResponse.builder()
                 .memberId(member.getId())
+                .nickname(member.getNickname())
+                .profileImgUrl(member.getProfileImg())
                 .accessToken(jwtToken.getAccessToken())
                 .refreshToken(jwtToken.getRefreshToken())
                 .build();
