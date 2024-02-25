ec86f33e51173d5d8481594da3bd964c2c78d281
gkfktkrh153 <gkfktkrh153@naver.com>
2024-02-25 19:23:29 +0900

    AsyncConfig 추가 및 @Async적용

--- src/main/java/com/api/farmingsoon/domain/notification/listener/NotificationSaveEventListener.java
+++ src/main/java/com/api/farmingsoon/domain/notification/listener/NotificationSaveEventListener.java
@@ -3,6 +3,8 @@ package com.api.farmingsoon.domain.notification.listener;
 import com.api.farmingsoon.common.sse.SseService;
 import com.api.farmingsoon.domain.notification.event.NotificationSaveEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -13,9 +15,9 @@ public class NotificationSaveEventListener {
 
     private  final SseService sseService;
 
+    @Async("testExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void sendNotification(NotificationSaveEvent event)
-    {
+    public void sendNotification(NotificationSaveEvent event) throws InterruptedException {
         event.getSenderIdList().forEach(sender -> sseService.sendToClient(sender, event.getMessage()));
     }
 }
