6be1a9ff8d162331db8edcb45a909ee066fe2bb3
gkfktkrh153 <gkfktkrh153@naver.com>
2024-02-27 17:36:37 +0900

    sse log 추가

--- src/main/java/com/api/farmingsoon/common/sse/SseService.java
+++ src/main/java/com/api/farmingsoon/common/sse/SseService.java
@@ -1,6 +1,7 @@
 package com.api.farmingsoon.common.sse;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -8,13 +9,14 @@ import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SseService {
     private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
     private final SseEmitterRepository sseEmitterRepository;
 
     public SseEmitter subscribe(Long userId) {
         SseEmitter emitter = createEmitter(userId);
-
+        log.info("구독 시도");
         sendToClient(userId, "EventStream Created. [userId=" + userId + "]");
         return emitter;
     }
@@ -24,7 +26,9 @@ public class SseService {
         SseEmitter emitter = sseEmitterRepository.get(receiverId);
         if (emitter != null) {
             try {
+                log.info("메시지 전송 전");
                 emitter.send(SseEmitter.event().id(String.valueOf(receiverId)).name("sse").data(data));
+                log.info("메시지 전송 후");
             } catch (IOException exception) {
                 sseEmitterRepository.deleteById(receiverId);
                 emitter.completeWithError(exception);
@@ -37,9 +41,17 @@ public class SseService {
         sseEmitterRepository.save(id, emitter);
 
         // Emitter가 완료될 때(모든 데이터가 성공적으로 전송된 상태) Emitter를 삭제한다.
-        emitter.onCompletion(() -> sseEmitterRepository.deleteById(id));
+        emitter.onCompletion(() ->
+        {
+            sseEmitterRepository.deleteById(id);
+            log.info("모든 데이터 전송 완료");
+        });
         // Emitter가 타임아웃 되었을 때(지정된 시간동안 어떠한 이벤트도 전송되지 않았을 때) Emitter를 삭제한다.
-        emitter.onTimeout(() -> sseEmitterRepository.deleteById(id));
+        emitter.onTimeout(() ->
+        {
+            sseEmitterRepository.deleteById(id);
+            log.info("타임아웃");
+        });
 
         return emitter;
     }
