bd5f6e055c71de9f41b512deda172c843a5d0da1
gkfktkrh153 <gkfktkrh153@naver.com>
2024-03-04 19:14:16 +0900

    DTO에 읽음 여부 추가

--- src/main/java/com/api/farmingsoon/domain/chat/dto/ChatResponse.java
+++ src/main/java/com/api/farmingsoon/domain/chat/dto/ChatResponse.java
@@ -15,13 +15,15 @@ public class ChatResponse {
     private String message;
     private Long chatId;
     private Long senderId;
+    private Boolean isRead;
     private LocalDateTime createAt;
 
     @Builder
-    private ChatResponse(Long senderId, String message,Long chatId, LocalDateTime createAt) {
+    private ChatResponse(Long senderId, String message, Boolean isRead, Long chatId, LocalDateTime createAt) {
         this.senderId = senderId;
         this.chatId = chatId;
         this.message = message;
+        this.isRead = isRead;
         this.createAt = createAt;
     }
     public static ChatResponse of(Chat chat) {
@@ -30,6 +32,7 @@ public class ChatResponse {
                 .senderId(chat.getSender().getId())
                 .chatId(chat.getId())
                 .message(chat.getMessage())
+                .isRead(chat.getIsRead())
                 .createAt(chat.getCreatedAt())
                 .build();
     }
