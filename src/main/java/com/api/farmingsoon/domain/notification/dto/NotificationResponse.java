1447effcf0ef183655ad8a89cb153515642d2917
gkfktkrh153 <gkfktkrh153@naver.com>
2024-03-26 21:07:29 +0900

    알림 response ID 추가

--- src/main/java/com/api/farmingsoon/domain/notification/dto/NotificationResponse.java
+++ src/main/java/com/api/farmingsoon/domain/notification/dto/NotificationResponse.java
@@ -10,17 +10,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class NotificationResponse {
     private Long itemId;
+    private Long notificationId;
     private String message;
 
     @Builder
-    private NotificationResponse(Long itemId, String message) {
+    private NotificationResponse(Long itemId, Long notificationId, String message) {
         this.itemId = itemId;
+        this.notificationId = notificationId;
         this.message = message;
     }
 
     public static NotificationResponse of(Notification notification){
         return NotificationResponse.builder()
                 .itemId(notification.getItemId())
+                .notificationId(notification.getId())
                 .message(notification.getMessage()).build();
     }
 }
