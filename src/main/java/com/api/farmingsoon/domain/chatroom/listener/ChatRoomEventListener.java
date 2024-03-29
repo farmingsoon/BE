68df956b3d4b12c250a52a5a32f0bf9e2d1c7c66
gkfktkrh153 <gkfktkrh153@naver.com>
2024-03-29 16:05:59 +0900

    참여 시 업데이트 알림 전송

--- src/main/java/com/api/farmingsoon/domain/chatroom/listener/ChatRoomEventListener.java
+++ src/main/java/com/api/farmingsoon/domain/chatroom/listener/ChatRoomEventListener.java
@@ -16,7 +16,7 @@ public class ChatRoomEventListener {
     private final ChatService chatService;
     private final ChatRoomRedisService chatRoomRedisService;
     private final SimpMessagingTemplate messagingTemplate;
-
+    private  final SseService sseService;
 
     /**
      * @Description
@@ -29,7 +29,7 @@ public class ChatRoomEventListener {
         chatRoomRedisService.connectChatRoom(event.getChatRoomId(), event.getSessionId());
         chatService.readAllMyNotReadChatList(event.getChatRoomId(), event.getConnectMemberId());
         messagingTemplate.convertAndSend("/sub/chat-room/" + event.getChatRoomId(), new ChattingConnectResponse(event.getConnectMemberId()));
-
+        sseService.sendToClient("CHATROOM_UPDATE", event.getConnectMemberId(), "채팅방 목록을 업데이트 해주세요.");
     }
 
     @EventListener
